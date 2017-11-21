0dfb37ab52b51ccfa9bf21f2db8d2570dbe94323
Piotrek Frącek <piotrekfracek@gmail.com>
2017-11-21 17:34:30 +0100

    Add hire us section

---
+++ src/components/HireUs/HireUs.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  .hire-us
+    br
+    br
+    br
+</template>
+
+<style src="./HireUs.sass" lang="sass" scoped></style>
--- src/pages/Services/Services.vue
+++ src/pages/Services/Services.vue
@@ -12,15 +12,20 @@
         - n++
     section.services__flexible
       - var n = 0;
-      while n < 30
+      while n < 40
         br
         - n++
+    section.services__hire-us: HireUs
 </template>
 
 <script>
   import head from 'src/head'
+  import HireUs from 'components/HireUs/HireUs'
 
   export default {
+    components: {
+      HireUs,
+    },
     head: head.set({
       title: () => 'Services',
     }),
