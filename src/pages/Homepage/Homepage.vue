1b1d4bdbca1c4f17a20b310c6cf5f73501e7f156
Piotrek Frącek <piotrekfracek@gmail.com>
2017-11-21 16:34:25 +0100

    Add homepage backgrounds

--- src/pages/Homepage/Homepage.vue
+++ src/pages/Homepage/Homepage.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
-  article
-    h1 Hello world!
+  article.homepage
+    section.homepage__landing
+      - var n = 0;
+      while n < 60
+        br
+        - n++
+    section.homepage__testimonials
+      - var n = 0;
+      while n < 40
+        br
+        - n++
+    section.homepage__contact
+      - var n = 0;
+      while n < 30
+        br
+        - n++
 </template>
 
 <script>
@@ -8,7 +22,9 @@
 
   export default {
     head: head.set({
-      title: () => 'home_title',
+      title: () => 'Home',
     }),
-  };
+  }
 </script>
+
+<style src="./Homepage.sass" lang="sass" scoped></style>
