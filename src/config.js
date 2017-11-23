f1b871ab7cae34075267160ef2054539cdb03d2f
Piotrek Frącek <piotrekfracek@gmail.com>
2017-11-23 14:03:38 +0100

    Remove config leftovers

--- src/config.js
+++ src/config.js
@@ -2,10 +2,8 @@ const env = process.env.NODE_ENV
 
 const config = {
   development: {
-    API_URL: 'http://localhost:4000',
   },
   production: {
-    API_URL: 'http://localhost:4000',
   },
 }
 
