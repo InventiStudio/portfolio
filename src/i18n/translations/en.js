f6a4df61f314b91becc28edff07e5961703ec0f8
Piotrek Frącek <piotrekfracek@gmail.com>
2017-11-27 15:54:46 +0100

    Add home and services content

--- src/i18n/translations/en.js
+++ src/i18n/translations/en.js
@@ -1,7 +1,95 @@
 export default {
-  pages: {
-    Homepage: {
-      title: 'Homepage',
+  common: {
+    orContactUs: 'or contact us!',
+  },
+  navbar: {
+    blog: 'Blog',
+  },
+  homepage: {
+    title: 'Home',
+    landing: {
+      title: 'We are Vue.js & Node.js web development agency based in Wrocław',
+      desc: 'We excel at building modern web applications, providing full-stack JavaScript and UI/UX design services for enterprises, companies and startups.',
+      cta: 'Estimate project',
     },
+    services: {
+      title: 'Services',
+      desc: 'What can we do for you?',
+    },
+    testimonials: {
+      title: 'Testimonials',
+      desc: 'Meet the people who trusted us',
+      cta: 'See case study',
+      cases: {
+        smokefree: {
+          author: 'Jon Doe',
+          position: 'CEO at DoeApp',
+          text: 'Lorem ipsum',
+        },
+      },
+    },
+  },
+  services: {
+    title: 'Services',
+    header: 'Our expertise',
+    desc: 'We provide Vue.js front-end, Node.js back-end and UI/UX design services. Whether you’re enterprise, company or startup - we can certainly help you!',
+    whatWeDo: {
+      title: 'What we do',
+      desc: 'The services we provide',
+    },
+    flexible: {
+      title: 'We are flexible',
+      desc: 'As an experienced developers we are open for different types of cooperation.',
+      technology: {
+        title: 'Different technology?',
+        desc: 'We\'ve been working with many technologies, so even if your project needs something that is missing in our services - do not hesitate to ask!',
+      },
+      subcontractor: {
+        title: 'Need us as a subcontractor?',
+        desc: 'We have experience working as subcontractors, so if you need us to do only some part of your project - feel free to contact us!',
+      },
+      project: {
+        title: 'Already started a project?',
+        desc: 'Do you have a project that needs to be continued? Or maybe you\'re a web developer and you need a help with unfamiliar technologies? No problem!',
+      },
+    },
+    card: {
+      cta: 'Read more',
+    },
+  },
+  vue: {
+    title: 'Vue.js front-end development',
+    shortTitle: 'Vue.js front-end',
+    desc: 'We are experts at creating beautiful and responsive front-end applications using Vue.js.',
+    tags: 'FRONT-END · VUE.JS · JAVASCRIPT · HTML5 · CSS3 · RESPONSIVE',
+    ctaEstimate: 'Estimate front-end project',
+  },
+  node: {
+    title: 'Node.js back-end development',
+    shortTitle: 'Node.js back-end',
+    desc: 'Need a powerful API? We make blazing fast Koa.js applications backed by PostgreSQL databases.',
+    tags: 'BACK-END · WEB APP · NODE.JS · KOA.JS · JAVASCRIPT · API · REST · POSTGRESQL',
+  },
+  design: {
+    title: 'UI & UX design',
+    shortTitle: 'UI & UX design',
+    desc: 'We love to design responsive and pixel-perfect user interfaces with top-notch user experience.',
+    tags: 'WEB DESIGN · UI · UX · RESPONSIVE · PROTOTYPE · WIREFRAME',
+  },
+  contact: {
+    title: 'Contact',
+    desc: 'Whether you want to hire us or simply get to know us better - feel free to get in touch!',
+    form: {
+      namePlaceholder: 'Your name (optional)',
+      emailPlaceholder: 'Your e-mail address',
+      messagePlaceholder: 'How can we help you?',
+      ctaSend: 'Send a message',
+    },
+  },
+  workWithUs: {
+    title: 'Want to work with us?',
+    desc: 'Whether you want to hire us or simply get to know us better - feel free to get in touch!',
+    ctaHire: 'Hire us',
+    ctaEstimate: 'Estimate project',
   },
 }
