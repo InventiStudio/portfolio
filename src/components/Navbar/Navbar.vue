487dc49d4653427dc2704901751ce64d2c856867
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-11-28 13:25:28 +0100

    Improve Navbar cta button

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -33,7 +33,7 @@
               active-class="navbar__link--active",
             ) Node
             a.navbar__link(href="https://blog.inventi.studio/", rel="nofollow") Blog
-            a.navbar__button(href="#")
+            a.navbar__cta.o-btn.transition(href="#")
               span.fs-16.c-white {{ $t('common.estimateProject') }}
 </template>
 
