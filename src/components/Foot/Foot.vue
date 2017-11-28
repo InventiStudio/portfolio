d365d316122f6771514855f10cb641e743586f69
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-11-28 20:41:51 +0100

    Finish Foot responsiveness

--- src/components/Foot/Foot.vue
+++ src/components/Foot/Foot.vue
@@ -2,9 +2,9 @@
   footer.foot.c-white-60
     .row.align-center
       .foot__column.small-12.large-4.columns
-        router-link.valign-middle(:to="{ name: 'Home' }")
+        router-link.align-middle(:to="{ name: 'Home' }")
           img(src="~assets/inventi__name.svg", alt="TODO")
-        a.foot__lang.o-link.c-white(href="#") Lang
+        a.o-link.c-white.ml-24.md-ml-40.align-middle(href="#") Lang
         nav.o-social-links.mt-24
           a(href="#"): icon(type="icon--facebook")
           a(href="#"): icon(type="icon--linkedin")
@@ -12,23 +12,16 @@
           a(href="#"): icon(type="icon--behance")
           a(href="#"): icon(type="icon--instagram")
           a(href="#"): icon(type="icon--github")
-      .foot__column.small-8.medium-4.large-2.columns
-        router-link.o-link.c-white.mb-8.block(:to="{ name: 'Home' }")
-          | {{ $t('home.title') }}
-        router-link.o-link.block(:to="{ name: 'Home', hash: '#services' }")
-          | {{ $t('home.services.title') }}
-        router-link.o-link.block(:to="{ name: 'Home', hash: '#testimonials' }")
-          | {{ $t('home.testimonials.title') }}
-        router-link.o-link.block(:to="{ name: 'Home', hash: '#contact' }")
-          | {{ $t('contact.title') }}
-      .foot__column.small-8.medium-4.large-2.columns
-        router-link.o-link.c-white.mb-8.block(:to="{ name: 'Services' }")
-          | {{ $t('services.title') }}
-        router-link.o-link.block(:to="{ name: 'Vue' }")
-          | {{ $t('vue.shortTitle') }}
-        router-link.o-link.block(:to="{ name: 'Node' }")
-          | {{ $t('node.shortTitle') }}
-      .foot__column.small-8.medium-4.large-2.columns
+      .foot__column.small-12.large-2.columns
+        router-link.o-link.c-white.mb-8.block(:to="{ name: 'Home' }") {{ $t('home.title') }}
+        router-link.o-link.block(:to="{ name: 'Home', hash: '#services' }") {{ $t('home.services.title') }}
+        router-link.o-link.block(:to="{ name: 'Home', hash: '#testimonials' }") {{ $t('home.testimonials.title') }}
+        router-link.o-link.block(:to="{ name: 'Home', hash: '#contact' }") {{ $t('contact.title') }}
+      .foot__column.small-12.large-2.columns
+        router-link.o-link.c-white.mb-8.block(:to="{ name: 'Services' }") {{ $t('services.title') }}
+        router-link.o-link.block(:to="{ name: 'Vue' }") {{ $t('vue.shortTitle') }}
+        router-link.o-link.block(:to="{ name: 'Node' }") {{ $t('node.shortTitle') }}
+      .foot__column.small-12.large-2.columns
         span.fs-16.c-white {{ $t('contact.title') }}
 </template>
 
