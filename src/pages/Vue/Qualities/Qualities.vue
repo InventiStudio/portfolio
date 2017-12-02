ef9ed82ade211d367791111eeb6b27e2ecb29659
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-02 20:06:16 +0100

    Simplify classes in Vue/Qualities

--- src/pages/Vue/Qualities/Qualities.vue
+++ src/pages/Vue/Qualities/Qualities.vue
@@ -5,20 +5,20 @@
         h1.o-heading-1.c-white {{ $t('vue.qualities.title') }}
         p.o-paragraph.c-white-60.mt-16.medium-mt-4.mb-64.max-w-560 {{ $t('vue.qualities.desc') }}
     .qualities__list.row.align-center
-      .qualities__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-white-50.mt-8(type="icon--trending-up")
+      .qualities__element.column.small-12.medium-6.large-3.mb-48
+        icon.c-white-50(type="icon--trending-up")
         h3.o-heading-3.c-white-90 {{ $t('vue.qualities.code.title') }}
         p.o-paragraph.c-white-60.mt-4 {{ $t('vue.qualities.code.desc') }}
-      .qualities__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-white-50.mt-8(type="icon--shield")
+      .qualities__element.column.small-12.medium-6.large-3.mb-48
+        icon.c-white-50(type="icon--shield")
         h3.o-heading-3.c-white-90 {{ $t('vue.qualities.tests.title') }}
         p.o-paragraph.c-white-60.mt-4 {{ $t('vue.qualities.tests.desc') }}
-      .qualities__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-white-50.mt-8(type="icon--smartphone")
+      .qualities__element.column.small-12.medium-6.large-3.mb-48
+        icon.c-white-50(type="icon--smartphone")
         h3.o-heading-3.c-white-90 {{ $t('vue.qualities.responsiveness.title') }}
         p.o-paragraph.c-white-60.mt-4 {{ $t('vue.qualities.responsiveness.desc') }}
-      .qualities__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-white-50.mt-8(type="icon--cpu")
+      .qualities__element.column.small-12.medium-6.large-3.mb-48
+        icon.c-white-50(type="icon--cpu")
         h3.o-heading-3.c-white-90 {{ $t('vue.qualities.technologies.title') }}
         p.o-paragraph.c-white-60.mt-4 {{ $t('vue.qualities.technologies.desc') }}
 </template>
