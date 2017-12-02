87d49beca58bfc2cc3a13c8e2790eb18b2d1c323
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-02 20:01:35 +0100

    Finish Services responsiveness

--- src/pages/Services/WeAreFlexible/WeAreFlexible.vue
+++ src/pages/Services/WeAreFlexible/WeAreFlexible.vue
@@ -4,15 +4,15 @@
       h1.o-heading-1.c-white {{ $t('services.flexible.title') }}
       p.o-paragraph.c-white-60.mt-16.medium-mt-4.mb-64.max-w-560 {{ $t('services.flexible.desc') }}
     ul.row.align-center.list-reset
-      li.column.small-12.large-3
+      li.column.small-12.medium-6.large-4.mb-48
         icon.c-white-50(type="icon--briefcase")
         h3.o-heading-3.c-white-90 {{ $t('services.flexible.technology.title') }}
         p.o-paragraph.c-white-60.mt-4 {{ $t('services.flexible.technology.desc') }}
-      li.column.small-12.large-3
+      li.column.small-12.medium-6.large-4.mb-48
         icon.c-white-50(type="icon--settings")
         h3.o-heading-3.c-white-90 {{ $t('services.flexible.subcontractor.title') }}
         p.o-paragraph.c-white-60.mt-4 {{ $t('services.flexible.subcontractor.desc') }}
-      li.column.small-12.large-3
+      li.column.small-12.medium-6.large-4
         icon.c-white-50(type="icon--fast-forward")
         h3.o-heading-3.c-white-90 {{ $t('services.flexible.project.title') }}
         p.o-paragraph.c-white-60.mt-4 {{ $t('services.flexible.project.desc') }}
