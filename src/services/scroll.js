5b08a7e49e5aa849b008c568e7f97cd9d456e202
Piotrek Frącek <piotrekfracek@gmail.com>
2017-12-02 23:09:04 +0100

    Do not scroll if element is not found

--- src/services/scroll.js
+++ src/services/scroll.js
@@ -1,8 +1,10 @@
 import scrollTo from 'scroll-to'
 
 export default function smoothScrollTo(target) {
-  scrollTo(0, document.getElementById(target).offsetTop, {
-    ease: 'inOutQuad',
-    duration: 800,
-  })
+  if (document.getElementById(target)) {
+    scrollTo(0, document.getElementById(target).offsetTop, {
+      ease: 'inOutQuad',
+      duration: 800,
+    })
+  }
 }
