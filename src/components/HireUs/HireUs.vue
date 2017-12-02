81b61f52d27656abeb2850136b29fd6adc759e6a
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-02 20:01:18 +0100

    Add o-btn--borderless for making borderless btns same size

--- src/components/HireUs/HireUs.vue
+++ src/components/HireUs/HireUs.vue
@@ -2,11 +2,11 @@
   section.hire-us
     .row
       .column.mb-32.large-mb-0
-        h1.o-heading-1.hire-us__title {{ $t('workWithUs.title') }}
-      .column.small-12.large-6.align-middle.hire-us__buttons
-        router-link.o-btn.text-center.hire-us__estimate(:to="{ name: 'Estimate' }")
+        h1.hire-us__title.o-heading-1 {{ $t('workWithUs.title') }}
+      .hire-us__buttons.column.small-12.large-6.align-middle
+        router-link.hire-us__estimate.o-btn.text-center.o-btn--borderless(:to="{ name: 'Estimate' }")
           span.fs-16 {{ $t('common.estimateProject') }}
-        router-link.o-btn.text-center.hire-us__contact(:to="{ name: 'Estimate' }")
+        router-link.hire-us__contact.o-btn.text-center(:to="{ name: 'Estimate' }")
           span.fs-16 {{ $t('common.orContactUs') }}
 </template>
 
