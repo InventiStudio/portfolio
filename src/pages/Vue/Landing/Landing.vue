d9b760d1c16309e408cbb634e11f3d3110a62f53
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-03 19:31:37 +0100

    Adjust Landings' white icons opacity

--- src/pages/Vue/Landing/Landing.vue
+++ src/pages/Vue/Landing/Landing.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   section.landing
     .row.column.text-center
-      icon.c-white-90.mb-32.medium-mb-40(type="icon--monitor")
+      icon.c-white-80.mb-32.medium-mb-40(type="icon--monitor")
       p.fs-11.c-white-60.mb-16.medium-mb-8 {{ $t('vue.tags') }}
       h1.o-heading-1.c-white {{ $t('vue.title') }}
       p.o-paragraph.c-white-60.mt-16.medium-mt-4.mb-40.max-w-560 {{ $t('vue.desc') }}
