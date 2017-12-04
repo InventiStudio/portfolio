459d52d543e5427cf6decca4646f2b2f29257b43
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-04 18:22:16 +0100

    Add basic structure for Estimae/Survey

--- src/pages/Estimate/Survey/Survey.vue
+++ src/pages/Estimate/Survey/Survey.vue
@@ -1,26 +1,210 @@
 <template lang="pug">
   section.survey
     form
-      h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q1.title') }}
-      span.o-heading-1.c-ship-gray-20 1/4
-      SurveyProgress(:step="1")
-      h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q2.title') }}
-      span.o-heading-1.c-ship-gray-20 2/4
-      SurveyProgress(:step="2")
-      h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q3.title') }}
-      span.o-heading-1.c-ship-gray-20 3/4
-      SurveyProgress(:step="3")
-      h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q4.title') }}
-      span.o-heading-1.c-ship-gray-20 4/4
-      SurveyProgress(:step="4")
+      .row.align-center.mb-16
+        header.survey__header.column.small-12.large-9
+          h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q1.title') }}
+          span.o-heading-1.c-ship-gray-20 1/4
+      .row.align-center.mb-32
+        .column.small-12.large-9
+          SurveyProgress(:step="1")
+      .row.align-center.mb-40.medium-mb-80
+        .column.small-12.large-9
+          .row.text-center
+            .column.small-12.medium-4
+              input.survey__input(type="radio", v-model="q1", id="q1a1", name="q1", value="a1")
+              label.survey__label.o-card.mb-24(for="q1a1")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--play")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q1.answers.a1') }}
+            .column.small-12.medium-4
+              input.survey__input(type="radio", v-model="q1", id="q1a2", name="q1", value="a2")
+              label.survey__label.o-card(for="q1a2")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--fast-forward")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q1.answers.a2') }}
+
+      .row.align-center.mb-16
+        header.survey__header.column.small-12.large-9
+          h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q2.title') }}
+          span.o-heading-1.c-ship-gray-20 2/4
+      .row.align-center.mb-32
+        .column.small-12.large-9
+          SurveyProgress(:step="2")
+      .row.align-center.mb-40.medium-mb-80
+        .column.small-12.large-9
+          .row.text-center
+            .column.small-12.medium-4
+              input.survey__input(type="checkbox", v-model="q2.a1", id="q2a1", name="q2", value="a1")
+              label.survey__label.o-card.mb-24(for="q2a1")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--monitor")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q2.answers.a1') }}
+            .column.small-12.medium-4
+              input.survey__input(type="checkbox", v-model="q2.a2", id="q2a2", name="q2", value="a2")
+              label.survey__label.o-card.mb-24(for="q2a2")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--server")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q2.answers.a2') }}
+            .column.small-12.medium-4
+              input.survey__input(type="checkbox", v-model="q2.a3", id="q2a3", name="q2", value="a3")
+              label.survey__label.o-card.mb-24(for="q2a3")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--grid")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q2.answers.a3') }}
+
+      .row.align-center.mb-16
+        header.survey__header.column.small-12.large-9
+          h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q3.title') }}
+          span.o-heading-1.c-ship-gray-20 3/4
+      .row.align-center.mb-32
+        .column.small-12.large-9
+          SurveyProgress(:step="3")
+      .row.align-center.mb-40.medium-mb-80
+        .column.small-12.large-9
+          .row.text-center
+            .column.small-12.medium-4
+              input.survey__input(type="checkbox", v-model="q3.a1", id="q3a1", name="q3", value="a1")
+              label.survey__label.o-card.mb-24(for="q3a1")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--monitor")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q3.answers.a1') }}
+            .column.small-12.medium-4
+              input.survey__input(type="checkbox", v-model="q3.a2", id="q3a2", name="q3", value="a2")
+              label.survey__label.o-card.mb-24(for="q3a2")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--smartphone")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q3.answers.a2') }}
+            .column.small-12.medium-4
+              input.survey__input(type="checkbox", v-model="q3.a3", id="q3a3", name="q3", value="a3")
+              label.survey__label.o-card.mb-24(for="q3a3")
+                .row.align-middle
+                  .column.small-4.medium-12.medium-mb-24
+                    icon.survey__icon(type="icon--file")
+                  .column.small-8.medium-12.text-left.medium-text-center
+                    h5.survey__heading-5.o-heading-3 {{ $t('estimate.survey.q3.answers.a3') }}
+
+      .row.align-center.mb-16
+        header.survey__header.column.small-12.large-9
+          h1.o-heading-1.c-ship-gray-80 {{ $t('estimate.survey.q4.title') }}
+          span.o-heading-1.c-ship-gray-20 4/4
+      .row.align-center.mb-32
+        .column.small-12.large-9
+          SurveyProgress(:step="4")
+      .row.align-center.mb-40.medium-mb-80
+        .column.small-12.large-9
+          .row.text-center
+            .column.small-12.medium-4
+              input.o-input(
+                type="text",
+                v-model="name",
+                :placeholder="$t('estimate.survey.q4.form.namePlaceholder')",
+                :class="{ 'o-input--error': !isNameValid }",
+              )
+              small.o-form-error
+                span(v-show="!isEmailValid") {{ $t('errors.name') }}
+            .column.small-12.medium-4
+              input.o-input(
+                type="text",
+                v-model="email",
+                :placeholder="$t('estimate.survey.q4.form.emailPlaceholder')",
+                :class="{ 'o-input--error': !isEmailValid }",
+              )
+              small.o-form-error
+                span(v-show="!isEmailValid") {{ $t('errors.email') }}
+            .column.small-12.medium-4
+              input.o-input(
+                type="text",
+                v-model="phone",
+                :placeholder="$t('estimate.survey.q4.form.phonePlaceholder')",
+              )
+            .column.small-12
+              textarea.o-textarea(
+                v-model="message",
+                :placeholder="$t('estimate.survey.q4.form.messagePlaceholder')",
+                :class="{ 'o-input--error': !isMessageValid }",
+              )
+              small.o-form-error
+                span(v-show="!isMessageValid") {{ $t('errors.message') }}
+            .column.small-12.text-center
+              button.survey__btn.o-btn(
+                type="button",
+                :disabled="!isFormValid",
+                @click="submit()",
+              )
+                span.fs-16.c-white {{ $t('estimate.survey.ctaSend') }}
 </template>
 
 <script>
+  import { required, email } from 'vuelidate/lib/validators'
   import SurveyProgress from 'components/SurveyProgress/SurveyProgress'
 
   export default {
     components: {
       SurveyProgress,
+    },
+    data() {
+      return {
+        q1: '',
+        q2: {
+          a1: false,
+          a2: false,
+          a3: false,
+        },
+        q3: {
+          a1: false,
+          a2: false,
+          a3: false,
+        },
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+      }
+    },
+    computed: {
+      isNameValid() { return !this.$v.name.$error },
+      isEmailValid() { return !this.$v.email.$error },
+      isMessageValid() { return !this.$v.message.$error },
+      isFormValid() {
+        return this.isNameValid && this.isEmailValid && this.isMessageValid
+      },
+    },
+    methods: {
+      submit() {
+        try {
+          if (this.$v.surveyForm.$touch() || this.$v.surveyForm.$error) return null
+          const { name, email, phone, message } = this
+          return { name, email, phone, message }
+        } catch (err) {
+          console.warn(err)
+          throw err
+        }
+      },
+    },
+    validations: {
+      name: { required },
+      email: { required, email },
+      message: { required },
+      surveyForm: [
+        'name',
+        'email',
+        'message',
+      ],
     },
   }
 </script>
