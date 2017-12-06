3650fa4fc738aaa360a844d89e3eee5c339abc5d
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-06 20:28:43 +0100

    Improve some icons

--- src/pages/Node/WhyWeChose/WhyWeChose.vue
+++ src/pages/Node/WhyWeChose/WhyWeChose.vue
@@ -6,19 +6,19 @@
         p.why-we-chose__desc.o-paragraph.c-ship-gray-40.mt-16.medium-mt-4.mb-64 {{ $t('node.whyWeChose.desc') }}
     .why-we-chose__list.row.align-center
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--globe")
+        icon.c-ship-gray-25.mt-8(type="icon--globe")
         h3.o-heading-3.c-ship-gray {{ $t('node.whyWeChose.ecosystem.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('node.whyWeChose.ecosystem.desc') }}
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--watch")
+        icon.c-ship-gray-25.mt-8(type="icon--watch")
         h3.o-heading-3.c-ship-gray {{ $t('node.whyWeChose.async.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('node.whyWeChose.async.desc') }}
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--box")
+        icon.c-ship-gray-25.mt-8(type="icon--box")
         h3.o-heading-3.c-ship-gray {{ $t('node.whyWeChose.modularity.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('node.whyWeChose.modularity.desc') }}
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--repeat")
+        icon.c-ship-gray-25.mt-8(type="icon--repeat")
         h3.o-heading-3.c-ship-gray {{ $t('node.whyWeChose.javascript.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('node.whyWeChose.javascript.desc') }}
 </template>
--- src/pages/Vue/WhyWeChose/WhyWeChose.vue
+++ src/pages/Vue/WhyWeChose/WhyWeChose.vue
@@ -6,19 +6,19 @@
         p.why-we-chose__desc.o-paragraph.c-ship-gray-40.mt-16.medium-mt-4.mb-64 {{ $t('vue.whyWeChose.desc') }}
     .why-we-chose__list.row.align-center
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--box")
+        icon.c-ship-gray-25.mt-8(type="icon--box")
         h3.o-heading-3.c-ship-gray {{ $t('vue.whyWeChose.component.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('vue.whyWeChose.component.desc') }}
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--feather")
+        icon.c-ship-gray-25.mt-8(type="icon--feather")
         h3.o-heading-3.c-ship-gray {{ $t('vue.whyWeChose.fastSmall.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('vue.whyWeChose.fastSmall.desc') }}
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--maximize-2")
+        icon.c-ship-gray-25.mt-8(type="icon--maximize-2")
         h3.o-heading-3.c-ship-gray {{ $t('vue.whyWeChose.progressive.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('vue.whyWeChose.progressive.desc') }}
       .why-we-chose__element.column.small-12.medium-6.large-3.mb-32
-        icon.c-ship-gray-20.mt-8(type="icon--globe")
+        icon.c-ship-gray-25.mt-8(type="icon--globe")
         h3.o-heading-3.c-ship-gray {{ $t('vue.whyWeChose.ecosystem.title') }}
         p.o-paragraph.c-ship-gray-40.mt-4 {{ $t('vue.whyWeChose.ecosystem.desc') }}
 </template>
