1ea00834eeb909e375acdbfe365eb6a2334a82d1
Piotrek Frącek <piotrekfracek@gmail.com>
2017-12-08 11:51:34 +0100

    Add some polish translations

--- src/content/i18n/pl.js
+++ src/content/i18n/pl.js
@@ -1,5 +1,56 @@
 export default {
+  meta: {
+    sitename: 'InventiStudio: Tworzenie aplikacji Vue.js i Node.js - Wrocław',
+    description: 'InventiStudio jest agencją programistyczną z Wrocławia. Jesteśmy ekspertami w tworzeniu aplikacji webowych z użyciem Vue.js i Node.js. Napisz do nas!',
+  },
+  common: {
+    inventiStudio: 'InventiStudio',
+    orContactUs: 'lub napisz do nas!',
+    estimateProject: 'Estymuj projekt',
+    previous: 'Poprzedni',
+    next: 'Następny',
+  },
+  navbar: {
+    title: 'Menu',
+    blog: 'Blog',
+  },
+  social: {
+    title: 'Nasze profile społecznościowe',
+  },
   home: {
     title: 'Strona główna',
+    landing: {
+      title: 'Jesteśmy agencją interaktywną tworzącą aplikacje Vue.js i Node.js',
+      desc: 'TODO: We excel at building modern web applications, providing full-stack JavaScript and UI/UX design services for enterprises, companies and startups.',
+      logo: 'Programiści Vue.js & Node.js',
+    },
+    services: {
+      title: 'Usługi',
+      desc: 'Co możemy dla Ciebie zrobić?',
+    },
+    testimonials: {
+      title: 'Referencje',
+      desc: 'Poznaj ludzi, którzy nam zaufali',
+      cta: 'Dowiedz się więcej',
+      cases: {
+        smokefree: {
+          author: 'Janusz Piechociński',
+          position: 'Szef w DoeApp',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a dictum ex. Aenean vitae consequat purus. Fusce tristique massa sit amet justo feugiat, at ullamcorper lectus euismod. Vestibulum ante metus, placerat non sem ac, suscipit mattis ligula. Quisque elementum tortor mauris, vel scelerisque neque ultrices sit amet. Donec a consequat lorem. Nullam tempor porttitor sem at volutpat.',
+        },
+      },
+    },
+    posts: {
+      title: 'Ostatnie publikacje',
+      desc: 'Dzielimy się wiedzą',
+      cta: 'Czytaj więcej',
+      cases: {
+        readability: {
+          title: 'Jak poprawiliśmy czytelność naszego funkcyjnego kodu',
+          desc: 'Jedną z ważniejszych zasad pozwalających na pisanie deterministycznego, łatwego w debugowaniu kodu, jest unikanie efektów ubocznych. Oczywistym wyborem, jeżeli chcemy owe założenie spełnić, jest korzystanie z paradygmatu funkcyjnego. Ten jednak niesie ze sobą zagrożenie w postaci nieczytelności kodu. Problem ten jest szczególnie widoczny w sytuacjach, w których na danych wejściowych wykonujemy serie transformacji.',
+          url: 'https://blog.inventi.studio/pl/jak-poprawilismy-czytelnosc-naszego-funkcyjnego-kodu/',
+        },
+      },
+    },
   },
 }
