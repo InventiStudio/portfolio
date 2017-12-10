7683f823e4a4fccfb46401b673fe1b7a17d38a40
Piotrek Frącek <piotrekfracek@gmail.com>
2017-12-10 14:51:14 +0100

    Add polish Vue page translations

--- src/content/i18n/pl.js
+++ src/content/i18n/pl.js
@@ -6,7 +6,7 @@ export default {
   common: {
     inventiStudio: 'InventiStudio',
     orContactUs: 'lub napisz do nas!',
-    estimateProject: 'Estymuj projekt',
+    estimateProject: 'Wyestymuj projekt',
     previous: 'Poprzedni',
     next: 'Następny',
   },
@@ -21,7 +21,7 @@ export default {
     title: 'Strona główna',
     landing: {
       title: 'Jesteśmy agencją interaktywną tworzącą aplikacje Vue.js i Node.js',
-      desc: 'TODO: We excel at building modern web applications, providing full-stack JavaScript and UI/UX design services for enterprises, companies and startups.',
+      desc: 'Specjalizujemy się w dostarczaniu nowoczesnych aplikacji internetowych opartych o technologię JavaScript oraz projektowaniu interfejsów graficznych.',
       logo: 'Programiści Vue.js & Node.js',
     },
     services: {
@@ -51,6 +51,107 @@ export default {
           url: 'https://blog.inventi.studio/pl/jak-poprawilismy-czytelnosc-naszego-funkcyjnego-kodu/',
         },
       },
+    },
+  },
+  services: {
+    title: 'Usługi',
+    desc: 'Tworzymy front-end we Vue.js, back-end w Node.js oraz zajmujemy się projektowaniem UI/UX. Niezależnie od tego, czy jesteś przedsiębiorcą, firmą czy startupem - z pewnością możemy Ci pomóc!',
+    landing: {
+      title: 'Nasz warsztat',
+    },
+    whatWeDo: {
+      title: 'Czym się zajmujemy',
+      desc: 'Usługi, które świadczymy',
+    },
+    flexible: {
+      title: 'Jesteśmy elastyczni',
+      desc: 'Jako doświadczeni programiści, jesteśmy otwarci na różne rodzaje współpracy.',
+      technology: {
+        title: 'Inna technologia?',
+        desc: 'Pracowaliśmy z wieloma technologiami, więc nawet, jeżeli Twój projekt wymaga czegoś o czym nie wspominamy na naszej stronie internetowej - śmiało zapytaj!',
+      },
+      subcontractor: {
+        title: 'Potrzebujesz podwykonawcy?',
+        desc: 'Mamy doświadczenie jako podwykonawcy, więc nawet jeżeli potrzebujesz nas jedynie jako części swojego zespołu, nie krępuj się z nami skontaktować!',
+      },
+      project: {
+        title: 'Już rozpoczęty projekt?',
+        desc: 'Prowadzisz projekt, który powinien być kontynuowany? Lub może sam jesteś programistą, ale potrzebujesz pomocy z technologiami, których nie znasz? Nie ma problemu!',
+      },
+    },
+    card: {
+      cta: 'Czytaj więcej',
+    },
+  },
+  vue: {
+    title: 'Front-end we Vue.js',
+    shortTitle: 'Vue.js front-end',
+    desc: 'Jesteśmy ekspertami w tworzeniu pięknych i responsywnych aplikacji front-endowych przy użyciu Vue.js.',
+    tags: 'FRONT-END · VUE.JS · JAVASCRIPT · HTML5 · CSS3 · RESPONSYWNOŚĆ',
+    ctaEstimate: 'Wyestymuj projekt front-end',
+    qualities: {
+      title: 'Zalety naszych front-endowych aplikacji',
+      desc: 'Wiemy, jak tworzyć świetne aplikacje HTML5. To cechy, które pozwalają nam to osiągnąć.',
+      code: {
+        title: 'Nowoczesny kod',
+        desc: 'Ze względu na naszą dbałość o jakość kodu, trzymamy się najnowszych standardów ECMAScript. Staramy się również, aby nasz kod był możliwie zgodny z paradygmatem funkcyjnym, co sprawia, że jest on modułowy i łatwy w utrzymaniu.',
+      },
+      tests: {
+        title: 'Testy',
+        desc: 'Wiemy, że dowody są silniejsze, niż przeczucia. Właśnie dlatego dostarczamy aplikacje sprawdzone w boju.',
+      },
+      responsiveness: {
+        title: 'Responsywność',
+        desc: 'W dzisiejszych czasach ludzie częściej korzystają z urządzeń mobilnych niż komputerów stacjonarnych. Wiemy o tym, dlatego nasze produkty są przygotowane na wszystkie rodzaje ekranów.',
+      },
+      technologies: {
+        title: 'Najlepsze technologie',
+        desc: 'Aby dostarczać najlepsze rozwiązania, używamy technologi front-endowych najwyższej klasy. Najważniejszą częścią naszego stosu jest framework Vue.js opisany poniżej. Oprócz tego używamy technologii Sass, Webpack, Yarn, Docker, Bootstrap i wielu innych!',
+      },
+    },
+    whyWeChose: {
+      title: 'Dlaczego wybraliśmy Vue.js',
+      desc: 'Vue.js to najlżejszy i najbardziej elastyczny framework JavaScript. Zobacz, dlaczego powinna to być główna technologia Twojej następnej aplikacji front-endowej!',
+      component: {
+        title: 'Komponentowy',
+        desc: 'We Vue.js wszystko jest komponentem. Budowanie aplikacji zorientowanych komponentowo umożliwia łatwe ponowne wykorzystanie już utworzonych elementów, dzięki czemu proces tworzenia zajmuje mniej czasu.',
+      },
+      fastSmall: {
+        title: 'Szybki i lekki',
+        desc: 'Vue.js waży tylko ~30KB i sprawi, że aplikacja będzie działać szybciej niż kiedykolwiek!',
+      },
+      progressive: {
+        title: 'Elastyczny',
+      desc: 'Sam Vue.js koncentruje się na warstwie widoku, mimo to łatwo dodać bilbioteki takie, jak Vuex lub Vue-router kiedy zwiększa sie złożoność aplikacji.',
+      },
+      ecosystem: {
+        title: 'Stabilny ekosystem',
+        desc: 'Zarówno Vue.js, jak i powiązane z nim biblioteki są rozwijane i utrzymywane przez ten sam główny zespół Vue.js.',
+      },
+    },
+    inventiAndVue: {
+      title: 'My + Vue.js =',
+      desc: 'Wiemy, jak tworzyć świetne rzeczy za pomocą Vue.js. Rzuć okiem na te źródła, aby zobaczyć, co możemy zrobić!',
+      rocket: 'Odlot! 🚀',
+      libraries: {
+        title: 'Nasze bilbioteki Vue.js',
+        desc: 'Jako część sporej społeczności Vue.js, napisaliśmy kilka bibliotek Vue.js, które pomagają nam i innym programistom tworzyć niezawodne produkty.',
+        cta: 'Znajdź nas na Githubie',
+      },
+      website: {
+        title: 'Nasza strona',
+        desc: 'Nasze portfolio napisane jest przy użyciu Vue.js i innych technologii, których używamy w komercyjnych projektach. Spójrz na nasz kod, jest otwarty!',
+        cta: 'Zobacz kod tej strony',
+      },
+      publications: {
+        title: 'Nasze publikacje',
+        desc: 'Od czasu do czasu piszemy o problemach, których doświadczyliśmy i rozwiązaniach, które znaleźliśmy. Dzielimy się zdobytą wiedzą na blogu.',
+        cta: 'Zobacz nasz blog',
+      },
+    },
+    technologies: {
+      title: 'Używamy najnowszych technologii',
+      desc: 'Narzędzia, które na co dzień używamy do tworzenia front-endu',
     },
   },
 }
