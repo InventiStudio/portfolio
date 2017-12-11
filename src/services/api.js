94d0282eb489a8a18338a792d0cfb55224b949f9
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-11 19:16:46 +0100

    Handle fetch errors

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,24 @@
 import config from 'src/config'
 
 function api(baseUrl) {
+  async function request(url, payload) {
+    const response = await fetch(url, payload)
+    if (!response.ok || (response.status >= 200 && response.status < 300)) {
+      throw Error(response.statusText)
+    } else {
+      return response
+    }
+  }
+
   async function post(path, body) {
     try {
-      return await fetch(`${baseUrl}${path}`, {
+      return request(`${baseUrl}${path}`, {
         method: 'POST',
         headers: new Headers({ 'Content-Type': 'application/json' }),
         body: JSON.stringify(body),
       })
     } catch (err) {
-      console.warn(err)
+      console.warn('api.post ', { err })
       throw err
     }
   }
--- src/services/mailer.js
+++ src/services/mailer.js
@@ -2,9 +2,14 @@ import constants from 'src/constants'
 import api from 'services/api'
 
 export default async function sendMail({ template_id, email, substitution_data, name = '' }) {
-  return api.post(constants.api.mailer, {
-    template_id,
-    recipients: [{ address: { email, name } }],
-    substitution_data,
-  })
+  try {
+    return api.post(constants.api.mailer, {
+      template_id,
+      recipients: [{ address: { email, name } }],
+      substitution_data,
+    })
+  } catch (err) {
+    console.warn('sendMail ', { err })
+    throw err
+  }
 }
