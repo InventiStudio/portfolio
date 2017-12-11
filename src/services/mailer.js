e36f0e1b05ccd4dd3edd072d152efb876779919c
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-11 17:58:38 +0100

    Adjust mailer service to api

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -3,7 +3,7 @@ import api from 'services/api'
 
 export default async function sendMail({ template_id, email, substitution_data, name = '' }) {
   return api.post(constants.api.mailer, {
-    content: { template_id },
+    template_id,
     recipients: [{ address: { email, name } }],
     substitution_data,
   })
