d7dce6b16db3cefab51e46b65baef090d8e16b12
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-11 20:57:34 +0100

    Remove webpack-merge from Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 
 COPY . /app
 
-RUN yarn add express compression sparkpost body-parser webpack-merge
+RUN yarn add express compression sparkpost body-parser
 
 EXPOSE ${PORT}
 
--- build/prod-server.js
+++ build/prod-server.js
@@ -5,7 +5,7 @@ const mailer = require('./mailer')
 const config = require('../config')
 
 const app = express()
-const port = process.env.PORT || config.build.port || 8000
+const port = process.env.PORT || 8000
 
 app.use(compression())
 app.use('/static', express.static(path.join(__dirname, '../dist', '/static')))
