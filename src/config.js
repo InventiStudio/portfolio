2744a0d0b4e6f73f095757da0653cc5415b7a481
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-11 17:13:09 +0100

    Add API_URLs to config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,13 @@
+import { build } from 'src/../config'
+
 const env = process.env.NODE_ENV
 
 const config = {
   development: {
+    API_URL: `http://localhost:${build.port}`,
   },
   production: {
+    API_URL: 'https://inventi.studio',
   },
 }
 
