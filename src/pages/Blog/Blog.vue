f530731a8f1c15f4a87cda456100a3ea6d7e3775
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-17 14:27:46 +0100

    Add Blog/Landing bg image

--- src/pages/Blog/Blog.vue
+++ src/pages/Blog/Blog.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
-  .blog
+  article.blog
+    section.blog__landing.o-wave--sinus
+      .row.column.text-center
+        .mb-32.medium-mb-40
+          icon.c-white-80(type="icon--feather")
+        h1.o-heading-1.c-white {{ $t('blog.title') }}
+        p.o-paragraph.c-white-60.mt-16.medium-mt-4.mb-40.max-w-560 {{ $t('blog.desc') }}
     div(v-for="post in posts", :key="post.data.slug")
       pre {{ post }}
       router-link.o-btn.o-btn--pink(:to="$routeByName('BlogPost', { params: { slug: post.data.slug } })") Go to
