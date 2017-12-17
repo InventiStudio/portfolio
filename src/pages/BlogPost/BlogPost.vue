337fda30081c0bef26f3b7642fa6e9f10f889ad6
Piotrek Frącek <piotrekfracek@gmail.com>
2017-12-17 12:39:33 +0100

    Add blog post page

---
+++ src/pages/BlogPost/BlogPost.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .blog-post
+    pre {{ post }}
+    div(v-html="post.html")
+
+</template>
+
+<script>
+  import { getBlogPostBySlug } from 'services/blog'
+
+  export default {
+    data() {
+      return {
+        post: [],
+      }
+    },
+    async mounted() {
+      this.post = await getBlogPostBySlug(this.$route.params.slug)
+    },
+  }
+</script>
+
+<style src="./BlogPost.sass" lang="sass" scoped></style>
