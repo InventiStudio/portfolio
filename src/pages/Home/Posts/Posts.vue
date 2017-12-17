87f7b53b47c1600652c32e76f83d976212ca3bce
Piotrek Frącek <piotrekfracek@gmail.com>
2017-12-17 17:44:37 +0100

    Update post's carousel. Add buttons

--- src/pages/Home/Posts/Posts.vue
+++ src/pages/Home/Posts/Posts.vue
@@ -4,15 +4,23 @@
       .column.small-12(:id="constants.sectionIds.home.posts")
         h2.o-heading-1.c-white {{ $t('home.posts.title') }}
         p.o-paragraph.c-white-60.mb-16 {{ $t('home.posts.desc') }}
+      .column.small-hidden.medium-1.large-2.align-middle
+        button.testimonial__button.testimonial__button--prev(
+          type="button",
+          @click="prevSlide()",
+          :class="{ 'testimonial__button--active': hasPrevSlide }",
+        )
+          icon(type="icon--arrow")
       .column.small-12.medium-10.large-8
         Carousel(
           :per-page="1",
           :min-swipe-distance="48",
           :pagination-enabled="true",
           :pagination-padding="12",
-          pagination-active-color="#f8188e",
-          pagination-color="#c9c9c9",
+          pagination-active-color="#ffffff",
+          pagination-color="#EEB3D4",
           :pagination-size="8",
+          ref="carousel",
         )
           Slide.small-p-16.medium-p-32(
             v-for="post in posts",
@@ -27,6 +35,13 @@
                   :to="$routeByName('BlogPost', { params: { slug: post.data.slug } })"
                 )
                   span.c-white.fs-16 {{ $t('home.posts.cta') }}
+      .column.small-hidden.medium-1.large-2.align-middle
+        button.testimonial__button.testimonial__button--next(
+          type="button",
+          @click="nextSlide()",
+          :class="{ 'testimonial__button--active': hasNextSlide }",
+        )
+          icon(type="icon--arrow")
 </template>
 
 <script>
@@ -42,6 +57,8 @@
     data() {
       return {
         posts: [],
+        hasPrevSlide: false,
+        hasNextSlide: false,
       }
     },
     computed: {
@@ -51,9 +68,27 @@
       getTranslation({ key }, property) {
         return this.$t(`home.posts.cases.${key}.${property}`)
       },
+      prevSlide() {
+        this.$refs.carousel.goToPage(this.$refs.carousel.getPreviousPage())
+      },
+      nextSlide() {
+        this.$refs.carousel.goToPage(this.$refs.carousel.getNextPage())
+      },
     },
     async mounted() {
       this.posts = await getAllBlogPosts()
+      this.$nextTick(() => {
+        this.hasPrevSlide = this.$refs.carousel.canAdvanceBackward
+        this.hasNextSlide = this.$refs.carousel.canAdvanceForward
+      })
+      this.$watch(
+        () => this.$refs.carousel.canAdvanceBackward,
+        canAdvanceBackward => (this.hasPrevSlide = canAdvanceBackward),
+      )
+      this.$watch(
+        () => this.$refs.carousel.canAdvanceForward,
+        canAdvanceForward => (this.hasNextSlide = canAdvanceForward),
+      )
     },
   }
 </script>
