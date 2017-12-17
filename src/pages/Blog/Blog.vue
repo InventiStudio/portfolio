447d55adbed66c9df68b3b14ba554ec9a8b2fb23
Piotrek Frącek <piotrekfracek@gmail.com>
2017-12-17 12:39:50 +0100

    Add blog -> blog post route

--- src/pages/Blog/Blog.vue
+++ src/pages/Blog/Blog.vue
@@ -1,10 +1,22 @@
 <template lang="pug">
- h1 Blog
+  .blog
+    div(v-for="post in posts", :key="post.data.permalink")
+      pre {{ post }}
+      router-link.o-btn.o-btn--pink(:to="$routeByName('BlogPost', { params: { slug: post.data.permalink } })") Go to
 </template>
 
 <script>
-  export default {
+  import { getAllBlogPosts } from 'services/blog'
 
+  export default {
+    data() {
+      return {
+        posts: [],
+      }
+    },
+    async mounted() {
+      this.posts = await getAllBlogPosts()
+    },
   }
 </script>
 
