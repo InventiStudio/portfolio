94b8789cba2d38bd3890b37233b894d55f6ec025
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-18 17:15:00 +0100

    Move date formatting to 'blog' service

--- src/services/blog.js
+++ src/services/blog.js
@@ -32,3 +32,12 @@ export async function getBlogPostBySlug(slug) {
   }
 }
 
+export function getFormattedDate(unformattedDate) {
+  const date = new Date(unformattedDate)
+  let dd = date.getDate()
+  let mm = date.getMonth() + 1
+  const yyyy = date.getFullYear()
+  if (dd < 10) dd = `0${dd}`
+  if (mm < 10) mm = `0${mm}`
+  return `${yyyy}-${mm}-${dd}`
+}
