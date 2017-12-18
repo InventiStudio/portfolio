83f62d0116771ec96710d3108d7af14df932a9bf
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-18 17:15:27 +0100

    Improve BlogCard responsiveness

--- src/components/BlogCard/BlogCard.vue
+++ src/components/BlogCard/BlogCard.vue
@@ -1,16 +1,20 @@
 <template lang="pug">
-  article.o-card.row.p-0.bg-white.overflow-hidden
-    .blog-card__content.column.small-12.medium-6
-      p.fs-11.c-ship-gray-40.mb-8 {{ dateFormatted }}
+  article.o-card.row.p-0.bg-white.overflow-hidden.u-no-nest
+    .blog-card__content.column.small-12.medium-6.small-order-2.medium-order-1
+      p.fs-11.c-ship-gray-40.mb-8 {{ formattedDate }}
       h2.fs-28.fw-black.c-ship-gray-80.mb-16.mt-0 {{ title }}
-      h3.o-paragraph.ff-lora.c-ship-gray-50.mb-32 {{ description }}
+      h3.o-paragraph.c-ship-gray-50.mb-24.medium-mb-32 {{ description }}
       router-link.o-link.c-pink(:to="$routeByName('BlogPost', { params: { slug } })")
         span.fs-16 {{ $t('common.readMore') }}
-    .column.small-12.medium-6.relative
-      .blog-card__cover(:style="{ backgroundImage: `url(/static/blog/${cover})` }")
+    .blog-card__cover.column.small-12.medium-6.relative.small-order-1.medium-order-2
+      router-link.blog-card__img(
+        :to="$routeByName('BlogPost', { params: { slug } })",
+        :style="{ backgroundImage: `url(/static/blog/${cover})` }",
+      )
 </template>
 
 <script>
+  import { getFormattedDate } from 'services/blog'
   import { openContactModal } from 'services/events'
 
   export default {
@@ -37,14 +41,8 @@
       },
     },
     computed: {
-      dateFormatted() {
-        const date = new Date(this.date)
-        let dd = date.getDate()
-        let mm = date.getMonth() + 1
-        const yyyy = date.getFullYear()
-        if (dd < 10) dd = `0${dd}`
-        if (mm < 10) mm = `0${mm}`
-        return `${yyyy}-${mm}-${dd}`
+      formattedDate() {
+        return getFormattedDate(this.date)
       },
     },
     methods: {
