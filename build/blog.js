b708300b9ef4dd92d39e34e67a886ca0a84fd91a
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-19 00:48:00 +0100

    Add synthax highlight plugin for markdown

--- build/blog.js
+++ build/blog.js
@@ -2,7 +2,17 @@ const files      = require('read-dir-files')
 const path       = require('path')
 const bodyParser = require('body-parser')
 const matter     = require('gray-matter')
-const markdown   = require('markdown-it')()
+const hljs       = require('highlight.js')
+const markdown   = require('markdown-it')({
+  highlight(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return `<pre class="hljs"><code>${hljs.highlight(lang, str, true).value}</code></pre>`
+      } catch (err) {}
+    }
+    return `<pre class="hljs"><code>${markdown.utils.escapeHtml(str)}</code></pre>`
+  },
+})
 
 const directory = lang => path.join(__dirname, '../', 'blog-posts', lang)
 
@@ -20,20 +30,19 @@ module.exports = function() {
           .map(filename => filename.replace(dir, ''))
           .map(filename => matter.read(path.join(dir, filename)))
           .map(({ content, data }) => ({ data, md: content }))
-          .sort((a,b) => new Date(b.data.date) - new Date(a.data.date))
+          .sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
         if (slug) {
           const post = posts.find(post => post.data.slug === slug)
           if (!post) return res.status(404).end()
           return res.end(JSON.stringify({
             data: post.data,
-            html: markdown.render(post.md)
+            html: markdown.render(post.md),
           }))
         } else {
-          return res.end(JSON.stringify(posts.map(({ data, md }) => ({ data }))))
+          return res.end(JSON.stringify(posts.map(({ data }) => ({ data }))))
         }
       })
-    }
+    },
   ]
-
 }
 
