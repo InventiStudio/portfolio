8ff62269754fa896494535c1905448bf1008641c
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2017-12-20 00:32:54 +0100

    Roarrr

--- src/components/Monster/Monster.vue
+++ src/components/Monster/Monster.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .monster
-    img(src="~assets/images/monster.svg")
+    img(src="~assets/images/monster.svg", alt="Roarrr!")
 </template>
 
 <style src="./Monster.sass" lang="sass" scoped></style>
