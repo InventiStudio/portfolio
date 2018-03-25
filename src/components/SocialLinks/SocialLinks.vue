018efe4246fb5384be36058b1c9d457bee8f7fd3
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-03-26 00:24:06 +0200

    Open social links in new tab

--- src/components/SocialLinks/SocialLinks.vue
+++ src/components/SocialLinks/SocialLinks.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   .social-links
-    a(:href="$t('content.urls.facebook')"):  icon(type="icon--facebook")
-    a(:href="$t('content.urls.linkedin')"):  icon(type="icon--linkedin")
-    a(:href="$t('content.urls.dribbble')"):  icon(type="icon--dribble")
-    a(:href="$t('content.urls.behance')"):   icon(type="icon--behance")
-    a(:href="$t('content.urls.instagram')"): icon(type="icon--instagram")
-    a(:href="$t('content.urls.github')"):    icon(type="icon--github")
+    a(:href="$t('content.urls.facebook')" target="_blank"):  icon(type="icon--facebook")
+    a(:href="$t('content.urls.linkedin')" target="_blank"):  icon(type="icon--linkedin")
+    a(:href="$t('content.urls.dribbble')" target="_blank"):  icon(type="icon--dribble")
+    a(:href="$t('content.urls.behance')" target="_blank"):   icon(type="icon--behance")
+    a(:href="$t('content.urls.instagram')" target="_blank"): icon(type="icon--instagram")
+    a(:href="$t('content.urls.github')" target="_blank"):    icon(type="icon--github")
 </template>
 
 <style src="./SocialLinks.sass" lang="sass" scoped></style>
